45948728eea726da6af5f7b241903943ecfeb975
Andrea <andrea9973@gmail.com>
2017-11-11 15:03:15 -0500

    FUCK YEAH NO STACK OVERFLOW

--- hero.py
+++ hero.py
@@ -1,11 +1,16 @@
-class hero(object):
-	def __init__(self, startX, startY):
+class HeroPlayer(object):
+	def __init__(self, startX, startY, startSize):
 		self.currX = startX
 		self.currY = startY 
+		self.size = startSize
 
-	def moveHero(dx, dy):
+	def moveHero(self, dx, dy):
 		currX += dx
 		currY += dy
 		if not isLegal(): pass
 
-	def isLegal(): pass
+	def isLegal(self): pass
+
+	def drawHero(self, canvas):
+		canvas.create_oval(self.currX, self.currY, self.currX + 10, self.currY + 10)
+
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import hero
 # Initialize data
 ####################################
 
-def init(data): pass
+def init(data):
+	data.player = hero.HeroPlayer(data.width*1/2, data.height*1/2, 1)
 
 ####################################
 # Define user interaction, timing
@@ -17,11 +18,17 @@ def keyPressed(event, data): pass
 
 def timerFired(data): pass
 
+def mouseMoved(event, data):
+	data.player.currX = event.x
+	data.player.currY = event.y
+	print(data.player.currX, data.player.currY)
+
 ####################################
 # Draw graphics
 ####################################
 
-def redrawAll(canvas, data): pass 
+def redrawAll(canvas, data):
+	data.player.drawHero(canvas)
 
 ####################################
 # Provided run function
@@ -48,12 +55,16 @@ def run(width=300, height=300):
         redrawAllWrapper(canvas, data)
         # pause, then call timerFired again
         canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+    def mouseMovedWrapper(event, data):
+    	mouseMoved(event, data)
+    	#redrawAllWrapper(canvas, data)
+
     # Set up data and call init
     class Struct(object): pass
     data = Struct()
     data.width = width
     data.height = height
-    data.timerDelay = 15
+    data.timerDelay = 50
     init(data)
     # create the root and the canvas
     root = Tk()
@@ -64,6 +75,8 @@ def run(width=300, height=300):
                             mousePressedWrapper(event, canvas, data))
     root.bind("<Key>", lambda event:
                             keyPressedWrapper(event, canvas, data))
+    root.bind('<Motion>', lambda event:
+    						mouseMovedWrapper(event, data))
     timerFiredWrapper(canvas, data)
     # and launch the app
     root.mainloop()  # blocks until window is closed
